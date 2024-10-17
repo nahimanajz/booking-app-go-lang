3180ff643c834bbf782bd21028b9a9e89dec9bad
nahimanajz <jazzonahimana@gmail.com>
2024-10-17 11:27:36 +0200

    go datatypes

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 go mod init <module-name>: it generates module and version of go that is being used
  ## executing application 
- `go run <filename>` 
+ `go run <filename>`
+ ## supported datatypes
+ - Strings, Integers, Booleans, Maps, Arrays 
+ `%v` let's you print the value of given variable and `%T` let you print the type of given datatype eg: `fmt.Printf("datatype is %T and value is %v", myVariable, myVariable)`
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 import "fmt"
 func main(){
-	fmt.Println("This is go")
+
+	var conferenceName = "Go conference"
+	var conferenceTickets = 50
+	var remainingTickets = 50;
+	
+	fmt.Println("Welcome to", conferenceName,"booking application")
+	fmt.Println("We have a total of", conferenceTickets, "ticket and ", remainingTickets ,"are still available.")
+	fmt.Println("Get your tickets here to attend")
+
+	// print formatting
+	fmt.Printf("Welcome to %v booking application \n remaining ticket %v \n ", conferenceName, remainingTickets)
+
+	var userName string
+	var userTickets int
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Printf("datatype is %T and value is %v \n", userName, userName)
+
 }
