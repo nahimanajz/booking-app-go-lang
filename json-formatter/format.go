0f50ff5f9572f598db4ac2c73270c0edd5ab9c03
nahimanajz <jazzonahimana@gmail.com>
2024-10-20 12:00:45 +0200

    Decoding json array

--- json-formatter/format.go
+++ json-formatter/format.go
@@ -12,9 +12,19 @@ type Bird struct {
 func main() {
 
 	birdJson := `{"species":"pigeon", "description":"likes to perch on rocks"}`
+	birdJsonArray :=`[
+		{"species":"pigeon", "description":"likes to perch on rocks"},
+		{"species":"eagle","description":"bird of prey"}
+	]`
 
 var bird Bird
+var birds []Bird
+
 json.Unmarshal([]byte(birdJson), &bird)
 fmt.Printf("Species: %s, Description: %s \n", bird.Species, bird.Description)
 
+//decoding array json
+
+json.Unmarshal([]byte(birdJsonArray), &birds)
+fmt.Printf("Birds: %+v \n", birds)
 }
